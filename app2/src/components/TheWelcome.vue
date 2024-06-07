df7d8a6bdeda10e86a5957d66f5e98d123c50159
Clodomir Jr. <106870350+jrfurt@users.noreply.github.com>
2024-06-07 08:11:48 -0300

    iniciando estudos de composition API

--- app2/src/components/TheWelcome.vue
+++ app2/src/components/TheWelcome.vue
@@ -4,16 +4,7 @@ import DocumentationIcon from './icons/IconDocumentation.vue';
 </script>
 
 <template>
-  <WelcomeItem>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
-    <template #heading>Documentation</template>
-
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener"
-      >official documentation</a
-    >
-    provides you with all information you need to get started.
-  </WelcomeItem>
+  <div>
+    <h1>Home</h1>
+  </div>
 </template>
--- app2/src/views/HomeView.vue
+++ app2/src/views/HomeView.vue
@@ -1,9 +1,25 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <main>
+      <TheWelcome />
+    </main>
+    <div>
+      {{ name.first_name }}
+      <button @click="changeName">Mudar Nome</button>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import TheWelcome from '../components/TheWelcome.vue';
+
+let name = ref({
+  first_name: 'Harry',
+  last_name: 'Potter'
+});
+
+const changeName = () => {
+  name.value.first_name = 'Ron';
+};
+</script>
