cec2afac7b590064fe77ecfcab95588823dc4eac
Clodomir Jr. <106870350+jrfurt@users.noreply.github.com>
2024-06-10 16:28:47 -0300

    estudando lifecycle hook e props e emit na composition api

---
+++ app2/src/components/AulaLifecicle.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Aula Lifecicle</h2>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onBeforeMount, onBeforeUnmount, onMounted, onUnmounted } from 'vue';
+
+onBeforeMount(() => {
+  console.log('on before mount');
+});
+onMounted(() => {
+  console.log('on mounted');
+});
+onBeforeUnmount(() => {
+  console.log('on before unmount');
+});
+onUnmounted(() => {
+  console.log('on unmounted');
+});
+</script>
--- app2/src/views/HomeView.vue
+++ app2/src/views/HomeView.vue
@@ -1,25 +1,90 @@
 <template>
   <div>
-    <main>
-      <TheWelcome />
-    </main>
     <div>
-      {{ name.first_name }}
-      <button @click="changeName">Mudar Nome</button>
+      <div>
+        <h2>Aula Ref</h2>
+        {{ name.first_name }}
+        <button @click="changeName">Mudar Nome</button>
+      </div>
+      <div>
+        <h2>Aula Computed e Watch</h2>
+        <div>
+          <h3>Computed</h3>
+          <ul>
+            <li>
+              A função <code>computed()</code> é utilizada quando um valor
+              depende de outro para sua renderização final
+            </li>
+          </ul>
+
+          <h3>Watch</h3>
+          <ul>
+            <li>
+              Já a função <code>watch()</code> é utilizando quando se precisa
+              observar alguma variável, então ele pode monitorá-la em tempo real
+            </li>
+            <li>
+              O primeiro parâmetro desta função é a variável que será
+              <em>observada</em> <br />
+              Seu segundo parâmetro é a callback function <br />
+              E, por último, mas opcional, as options, que é um objeto podendo
+              conter a função <code>deep: true</code>. por exemplo, quando se
+              observa um objeto
+            </li>
+          </ul>
+          <br />
+          Full name: {{ fullName }}
+          <p>Watch: {{ name.first_name }}</p>
+        </div>
+      </div>
+
+      <div>
+        <AulaLifecicle v-if="showComponent" />
+        <button @click="showComponent = !showComponent">Liga/Desliga</button>
+      </div>
+      <AulaPropsEmit />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import TheWelcome from '../components/TheWelcome.vue';
+import AulaLifecicle from '@/components/AulaLifecicle.vue';
+import AulaPropsEmit from '@/components/AulaPropsEmit.vue';
+import { computed, ref, watch } from 'vue';
 
 let name = ref({
   first_name: 'Harry',
   last_name: 'Potter'
 });
 
+const showComponent = ref(true);
+
 const changeName = () => {
   name.value.first_name = 'Ron';
 };
+
+const fullName = computed(() => {
+  return `${name.value.first_name} ${name.value.last_name}`;
+});
+
+watch(
+  name,
+  () => {
+    console.log('Eu tô observando!');
+  },
+  // options
+  // só é utilizada quando eu preciso observar um objeto inteiro
+  // para uma variável simples ele não é necessário
+  { deep: true }
+);
 </script>
+
+<style>
+h2 {
+  font-weight: 500;
+}
+
+div {
+  margin-bottom: 1rem;
+}
+</style>
