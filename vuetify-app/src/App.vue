ed4403e7ece3af463232294476ea895cc13feb0a
Clodomir Jr. <106870350+jrfurt@users.noreply.github.com>
2024-06-13 16:03:58 -0300

    add nav bar and app bar to App.vue

--- vuetify-app/src/App.vue
+++ vuetify-app/src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <v-app>
+    <v-navigation-drawer v-model="drawer"></v-navigation-drawer>
+    <v-app-bar flat color="blue-grey-lighten-5" class="border-b">
+      <v-app-bar-nav-icon @click="drawer = !drawer" ></v-app-bar-nav-icon>
+      <v-app-bar-title>Application</v-app-bar-title>
+    </v-app-bar>
     <v-main>
       <HelloWorld />
     </v-main>
@@ -7,5 +12,7 @@
 </template>
 
 <script setup lang="ts">
-  //
+import { ref } from 'vue';
+
+const drawer = ref<boolean>(false);
 </script>
