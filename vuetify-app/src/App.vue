189b605ac8e39ce1ef1e75f4ad7895d69d93f632
Clodomir Jr. <106870350+jrfurt@users.noreply.github.com>
2024-06-13 16:28:09 -0300

    create CardTeste.vue component

--- vuetify-app/src/App.vue
+++ vuetify-app/src/App.vue
@@ -2,17 +2,18 @@
   <v-app>
     <v-navigation-drawer v-model="drawer"></v-navigation-drawer>
     <v-app-bar flat color="blue-grey-lighten-5" class="border-b">
-      <v-app-bar-nav-icon @click="drawer = !drawer" ></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-app-bar-title>Application</v-app-bar-title>
     </v-app-bar>
     <v-main>
+      <CardTeste />
       <HelloWorld />
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from "vue";
 
 const drawer = ref<boolean>(false);
 </script>
---
+++ vuetify-app/src/components/CardTeste.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-container>
+    <div style="width: 100px; height: 100px;" class="border">
+      teste
+    </div>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+  //
+</script>
