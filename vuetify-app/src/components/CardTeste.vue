046cf2e480a32497c3c0ccbd474df04d96de28ed
Clodomir Jr. <106870350+jrfurt@users.noreply.github.com>
2024-06-17 15:35:21 -0300

    estudando display flex e breakpoints

--- vuetify-app/src/components/CardTeste.vue
+++ vuetify-app/src/components/CardTeste.vue
@@ -1,13 +1,16 @@
 <template>
   <v-container>
-    <div class="d-flex justify-space-around">
-      <div style="width: 100px; height: 100px;" class="border rounded elevation-1 pa-4">
+    <div class="d-sm-flex">
+      <div class="w-25 border rounded elevation-1 pa-4">
         teste
       </div>
-      <div style="width: 100px; height: 100px;" class="border rounded elevation-1 pa-4">
+      <div  class="w-25 border rounded elevation-1 pa-4">
         teste
       </div>
-      <div style="width: 100px; height: 100px;" class="border rounded elevation-1 pa-4">
+      <div class="w-25 border rounded elevation-1 pa-4">
+        teste
+      </div>
+      <div class="w-25 border rounded elevation-1 pa-4">
         teste
       </div>
     </div>
