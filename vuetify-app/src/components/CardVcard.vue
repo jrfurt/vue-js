fddcae8c36c19eaf7c5169448f7091270d3fc453
Clodomir Jr. <106870350+jrfurt@users.noreply.github.com>
2024-06-18 15:41:51 -0300

    estudando os diversos tipos de formas de criação de cards a partir do v-card

--- vuetify-app/src/components/CardVcard.vue
+++ vuetify-app/src/components/CardVcard.vue
@@ -1,13 +1,67 @@
 <template>
   <v-container>
     <v-row>
-      <v-col sm="6">
+      <v-col sm="12" md="6" lg="4">
         <v-card
+          variant="tonal"
           title="Pets"
-          text="Este é um card feito com um componente v-card"
+          subtitle="Este é um subtítulo"
+          text="Este é um card feito com um componente v-card utilizando props"
+          hover
         >
+          <v-card-actions>
+            <v-btn> Ver Mais </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col sm="12" md="6" lg="4">
+        <v-card>
+          <v-card-item>
+            <v-card-title>Camaleão</v-card-title>
+            <v-card-subtitle>Réptil</v-card-subtitle>
+          </v-card-item>
+          <v-card-text>
+            Este é outro card criado utilizando v-card, agora utilizando tags
+          </v-card-text>
+          <v-card-actions>
+            <v-btn> Ver Mais </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col sm="6" lg="4">
+        <v-card>
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+          ></v-img>
+          <v-card-title>Flores</v-card-title>
+          <v-card-subtitle>Argônias</v-card-subtitle>
+
+          <v-card-actions>
+            <v-btn color="blue" text="Saiba mais"></v-btn>
+            <v-spacer></v-spacer>
+            <v-btn
+              :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+              @click="show = !show"
+            ></v-btn>
+          </v-card-actions>
+
+          <v-expand-transition>
+            <div v-show="show">
+              <v-divider></v-divider>
+
+              <v-card-text>
+                Este é mais um card criado utilizando outras propriedades
+              </v-card-text>
+            </div>
+          </v-expand-transition>
         </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const show = ref<boolean>(false);
+</script>
